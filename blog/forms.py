f6520504b427d08b86a2206d1994ba40e2a591ed
tuannd <tuan.nd@zinza.com.vn>
2023-03-27 11:46:46 +0700

    Add css sample

--- blog/forms.py
+++ blog/forms.py
@@ -4,8 +4,9 @@ from .models import Comment
 
 class CommentForm(forms.ModelForm):
     # comment = forms.CharField(widget=forms.Textarea)
-    content = forms.CharField(widget=TinyMCE(attrs={'rows': 10}))
+    content = forms.CharField(widget=TinyMCE(attrs={'rows': 10}), label='Comment')
     class Meta:
         model  = Comment
         fields = ['content']
         exclude= ['post', 'author']
+        
